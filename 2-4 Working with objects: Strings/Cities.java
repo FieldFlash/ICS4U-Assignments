9d9fca1a3139a779067a7f3d754cd20eb3e1926a
FieldFlash <134171827+FieldFlash@users.noreply.github.com>
2024-10-01 14:56:45 +0000

    e

---
+++ 2-4 Working with objects: Strings/Cities.java
@@ -0,0 +1,5 @@
+public class Cities {
+    public static void main(String[] args){
+        
+    }
+}
--- 2-4 Working with objects: Strings/CompoundInterest.java
+++
@@ -1,5 +0,0 @@
-public class CompoundInterest {
-    public static void main(String[] args){
-        System.out.println("alex sherman")
-    }
-}
