96de0ac8ea347309e9f7767ae718a8e2bc5b2fb8
FieldFlash <134171827+FieldFlash@users.noreply.github.com>
2024-10-01 15:14:35 +0000

    organized

---
+++ 2.4 Working with Strings/Cities.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Cities {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String city;
+        String longest = "";
+        int count = 0;
+        do {
+            System.out.print("Enter a city name (or 'done' to stop): ");
+            city = input.nextLine();
+            if (city.length() > longest.length()) {
+                longest = city.toUpperCase();
+            }
+            count++;
+        } while (!city.equals("done"));
+        input.close();
+        System.out.println((count-1)+" names were entered, the longest was "+longest+"!");    }
+}
+
