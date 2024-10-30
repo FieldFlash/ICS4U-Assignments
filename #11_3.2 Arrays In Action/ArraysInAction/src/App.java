28552f4677a269e1532d8299678c8112d1f4e365
Liam <liam@shelston.com>
2024-10-30 10:55:40 -0400

    Added work files
    Added 3.2 Arrays In Action

--- #11_3.2 Arrays In Action/ArraysInAction/src/App.java
+++ #11_3.2 Arrays In Action/ArraysInAction/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[5];
+        int i = 0;
+        int num = 0;
+
+        while (true) {
+            try {
+                System.out.print("Enter a number (or -1 to exit): ");
+                num = input.nextInt();
+                if (num == -1) {
+                    System.out.println("Exiting program");
+                    input.close();
+                    System.out.println("Numbers: " + Arrays.toString(numbers));
+                    break;
+                }
+                numbers[i] = num;
+                i++;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Resizing array...");
+                numbers = new int[numbers.length + 5];
+            }
+        }
     }
 }
