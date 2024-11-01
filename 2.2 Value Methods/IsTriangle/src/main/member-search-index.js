c820ce169c7151fdfefa11f8b039cd75969f27b7
Liam <liam@shelston.com>
2024-11-01 11:02:09 -0400

    Removed html

--- 2.2 Value Methods/IsTriangle/src/main/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"main","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"main","c":"App","l":"isTriangle(int, int, int)","u":"isTriangle(int,int,int)"},{"p":"main","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
